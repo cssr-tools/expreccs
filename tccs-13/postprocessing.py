258a7c03973fca43c9a2396c27f8525946fda6e6
David Landa Marban <dmar@norceresearch.no>
2025-11-12 18:05:07 +0100

    Updating the tccs-13 docs and scripts

--- tccs-13/postprocessing.py
+++ tccs-13/postprocessing.py
@@ -8,7 +8,6 @@ Script to postprocess everest optimization (differential_evolution) studies.
 
 import argparse
 import json
-import csv
 import os
 import numpy as np
 import matplotlib
@@ -35,7 +34,7 @@ plt.rcParams.update(
 def postprocessing():
     """Main function to postprocess everest results (differential_evolution)"""
     cmdargs = load_parser()
-    dic = {"folder": f"{os.getcwd()}/{cmdargs['folder'].strip()}"}
+    dic = {"folder": os.path.abspath(cmdargs['folder'].strip())}
     with open(f"{dic['folder']}/configuration.json", "r", encoding="utf-8") as f:
         dic = {**json.load(f), **dic}
 
@@ -43,80 +42,68 @@ def postprocessing():
     if not os.path.exists(f"{dic['folder']}/figures"):
         os.system(f"mkdir {dic['folder']}/figures")
 
+    dic["ind_batch"], dic["ind_sim"] = [0, 0], [0, 0]
+    for i in range(4):
+        dic[f"s{i}"] = []
+        dic[f"x{i}"] = 0
+    process_optimization_results(dic)
     plot_optimization_results(dic)
     plt.rcParams.update({"axes.grid": False})
     plot_optimization_details(dic)
     find_optimal(dic)
 
 
+def process_optimization_results(dic):
+    """Process the optimization results over steps (batches)"""
+    os.chdir(dic["folder"])
+    dic["tot_eval"], dic["optimal"], dic["optimization"], where = 0, [], [], []
+    improved = -np.inf
+    root = "everest_output/sim_output"
+    for n in range(len(os.listdir(root))):
+        for m in range(len(os.listdir(f"{root}/batch_{n}/realization_0"))):
+            file = root + f"/batch_{n}/realization_0/evaluation_{m}/func"
+            if os.path.exists(file):
+                value = np.genfromtxt(file)
+                dic["tot_eval"] += 1
+                if value == -1e3:
+                    dic["x0"] += 1
+                elif value == -1e2:
+                    dic["x1"] += 1
+                elif value < 0:
+                    dic["x2"] += 1
+                else:
+                    dic["x3"] += 1
+                if value > -10:
+                    improved = max(improved, value)
+                    dic["optimal"].append(value)
+                    where.append([n, m])
+        for i in range(4):
+            dic[f"s{i}"].append(dic[f"x{i}"])
+            dic[f"x{i}"] = 0
+        dic["optimization"].append(improved)
+    ind = int(np.nanargmax(np.array(dic["optimal"])))
+    dic["optimal"] = dic["optimal"][ind]
+    dic["ind_batch"][1] = where[ind][0]
+    dic["ind_sim"][1] = where[ind][1]
+
+
 def find_optimal(dic):
     """Find the well locations and folder for the 'optimal' obtained solution"""
-    file = f"{dic['folder']}/everest_output/optimization_output/optimizer_output.txt"
-    with open(file, "r", encoding="utf8") as file:
-        optimal = -np.float16((file.readlines()[-1].strip()).split()[4])
-    file = f"{dic['folder']}/everest_output/optimization_output/results.txt"
-    dic["ind_batch"] = [0, 0]
-    dic["ind_sim"] = [0, 0]
-    batch_size = 0
-    with open(file, "r", encoding="utf8") as file:
-        for row in csv.reader(file):
-            if len(row) > 0:
-                if ((row[0].strip()).split()[0]).isdigit():
-                    if int((row[0].strip()).split()[1]) == 0:
-                        batch_size += 1
-                    if np.float16((row[0].strip()).split()[2]) == optimal:
-                        dic["ind_batch"][1] = int(
-                            int((row[0].strip()).split()[0]) / batch_size
-                        )
-                        dic["ind_sim"][1] = (
-                            int((row[0].strip()).split()[0])
-                            - dic["ind_batch"][1] * batch_size
-                        )
-                        break
+    
     for i, name in enumerate(["initial_guess", "optimal_solution"]):
         if not os.path.exists(f"{dic['folder']}/figures/{name}"):
             os.system(f"mkdir {dic['folder']}/figures/{name}")
         os.chdir(f"{dic['folder']}/figures/{name}")
         os.system(
             f"cp {dic['folder']}/everest_output/sim_output/"
-            + f"batch_{dic['ind_batch'][i]}/geo_realization_0/simulation_"
+            + f"batch_{dic['ind_batch'][i]}/realization_0/evaluation_"
             + f"{dic['ind_sim'][i]}/point_0.json ."
         )
         os.system(f"python3 {dic['folder']}/jobs/locations.py --mode post")
 
 
-def read_results(dic):
-    """Get the values over the optimization steps"""
-    file = f"{dic['folder']}/everest_output/optimization_output/results.txt"
-    n, dic["tot_eval"] = 0, 0
-    with open(file, "r", encoding="utf8") as file:
-        for row in csv.reader(file):
-            if len(row) > 0:
-                if ((row[0].strip()).split()[0]).isdigit():
-                    dic["tot_eval"] += 1
-                    if n != int((row[0].strip()).split()[1]):
-                        for i in range(4):
-                            dic[f"s{i}"].append(dic[f"x{i}"])
-                            dic[f"x{i}"] = 0
-                        n += 1
-                    if float((row[0].strip()).split()[2]) == -1e20:
-                        dic["x0"] += 1
-                    elif float((row[0].strip()).split()[2]) == -1e10:
-                        dic["x1"] += 1
-                    elif float((row[0].strip()).split()[2]) < 0:
-                        dic["x2"] += 1
-                    else:
-                        dic["x3"] += 1
-    for i in range(4):
-        dic[f"s{i}"].append(dic[f"x{i}"])
-
-
 def plot_optimization_details(dic):
     """Plot the number of failed and succeed simulations over steps"""
-    for i in range(4):
-        dic[f"s{i}"] = []
-        dic[f"x{i}"] = 0
-    read_results(dic)
     colors = ["k", "m", "r", "g", "b"]
     names = [
         f"Failed (well on inactive region, no={sum(dic['s0'])})",
@@ -152,31 +139,22 @@ def plot_optimization_details(dic):
 
 def plot_optimization_results(dic):
     """Plot the optimization values over steps"""
-    optimization = []
-    file = f"{dic['folder']}/everest_output/optimization_output/optimizer_output.txt"
-    with open(file, "r", encoding="utf8") as file:
-        for j, row in enumerate(csv.reader(file)):
-            optimization.append(
-                [j + 1, -float((row[0].strip()).split()[4])]
-            )
     fig, axis = plt.subplots()
     axis.step(
-        [step[0] for step in optimization],
-        [value[1] for value in optimization],
+        range(1, len(dic["optimization"]) + 1),
+        dic["optimization"],
         lw=5,
         color="b",
     )
     axis.set_title("Optimization results")
     axis.set_ylabel("min[(p$_{lim}$-p)/p$_{lim}$] [-]")
     axis.set_xlabel(r"Step [\#]")
-    print(optimization)
-    exit()
-    if optimization[-1][0] + 1 < 20:
+    if len(dic["optimization"]) < 20:
         axis.xaxis.set_major_locator(MaxNLocator(integer=True))
     else:
         axis.set_xticks(np.linspace(
             0,
-            optimization[-1][0] + 1,
+            len(dic["optimization"]) + 1,
             7,
         ))
     fig.savefig(
